bea2538a0c171a2eb750795ee50a465029239f26
Mary Ragheb <ragheb.m@northeastern.edu>
2024-10-23 16:36:44 -0400

    implementation

--- src/main/java/org/example/MyCircularQueue.java
+++ src/main/java/org/example/MyCircularQueue.java
@@ -1,31 +1,71 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MyCircularQueue {
+    Integer[] que;//Array<Integer> que;
+    int front;
+    int rear;
+    int size;
+
     public MyCircularQueue(int k) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        que = new Integer[k];
+        front = -1;
+        rear = -1;
+        size = k;
     }
 
     public boolean enQueue(int value) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            front++;
+        }
+        rear++;
+        if(rear == size) {
+            rear = 0;
+        }
+        que[rear] = value;
+
+        return true;
+
+        //throw new UnsupportedOperationException("Not implemented yet");
     }
 
     public boolean deQueue() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if(isEmpty()) {
+            return false;
+        }
+        front++;
+        if (front == size) {
+            front = -1; //0;
+        }
+        return true;
     }
 
     public int Front() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (isEmpty()) return -1;
+        return que[front];
+
     }
 
     public int Rear() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if(isEmpty()) {
+            return -1;
+        }
+        return que[rear];
+
     }
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return front == -1;//rear < front || front == -1;
     }
 
     public boolean isFull() {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        return rear >= size-1;
+        //throw new UnsupportedOperationException("Not implemented yet");
     }
 }
